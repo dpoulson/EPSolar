21a01ec8735eb06e34736fab8f0b268b6ca11061
Darren Poulson <darren.poulson@gmail.com>
2017-07-28 19:03:01 +0100

    Some fixes, clean ups, and MQTT debugging

--- EPSolar.ino
+++ EPSolar.ino
@@ -4,19 +4,20 @@
 #include <ArduinoOTA.h>
 #include <SimpleTimer.h>
 #include <ModbusMaster.h>
-#include "settings.h"
 #include <PubSubClient.h>
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 
-int timerTask1, timerTask2, timerTask3;
-float battBhargeCurrent, bvoltage, btemp, bremaining, lpower, lcurrent, pvvoltage, pvcurrent, pvpower;
-float stats_today_pv_volt_min, stats_today_pv_volt_max;
+int timerTask2, timerTask3;
+float battBhargeCurrent, btemp, bremaining, lpower, lcurrent, pvvoltage, pvcurrent, pvpower;
 uint8_t result;
 char* ctemp;
+char* bvoltage;
 
 const char* ssid = "60 Chequers Avenue AP1";
 const char* password = "trial3211";
 const char* mqtt_server = "192.168.6.46";
+
+#define OTA_HOSTNAME                    "SOLAR-CHARGE-MONITOR-1"
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -38,7 +39,6 @@ typedef void (*RegistryList[])();
 RegistryList Registries = {
   AddressRegistry_3100,
   AddressRegistry_311A,
-  AddressRegistry_3300,
 };
 // keep log of where we are
 uint8_t currentRegistryNumber = 0;
@@ -68,7 +68,7 @@ void setup() {
   ArduinoOTA.setHostname(OTA_HOSTNAME);
 
   // No authentication by default
-  ArduinoOTA.setPassword((const char *)"123");
+  //ArduinoOTA.setPassword((const char *)"123");
 
   ArduinoOTA.onStart([]() {
     Serial.println("Start");
@@ -88,10 +88,6 @@ void setup() {
     else if (error == OTA_END_ERROR) Serial.println("End Failed");
   });
   ArduinoOTA.begin();
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
   client.setServer(mqtt_server, 1883);
 
 
@@ -107,11 +103,8 @@ void reconnect() {
     if (client.connect("EPSolar1")) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("solar", "test");
+      client.publish("EPSolar/1/loop1", "Reconnect");
     } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
       delay(5000);
     }
@@ -123,56 +116,29 @@ void doRegistryNumber() {
 }
 
 void AddressRegistry_3100() {
-  Serial.println("Registry 3100");
+  client.publish("EPSolar/1/loop1", "loop");
   result = node.readInputRegisters(0x3100, 7);
   if (result == node.ku8MBSuccess)
   {
+    client.publish("EPSolar/1/loop1", "result");
     dtostrf((node.getResponseBuffer(0x11) / 100.0f), 6, 3, ctemp );
     client.publish("EPSolar/1/temp1", ctemp);
-    if (debug == 1) {
-      Serial.print("Temperature: ");
-      Serial.println(ctemp);
-    }
-    bvoltage = node.getResponseBuffer(0x04) / 100.0f;
-    if (debug == 1) {
-      Serial.print("Battery Voltage: ");
-      Serial.println(bvoltage);
-    }
+
+    dtostrf((node.getResponseBuffer(0x04) / 100.0f), 6, 3, bvoltage );
+    client.publish("EPSolar/1/bvoltage", bvoltage);
+
     lpower = ((long)node.getResponseBuffer(0x0F) << 16 | node.getResponseBuffer(0x0E)) / 100.0f;
-    if (debug == 1) {
-      Serial.print("Load Power: ");
-      Serial.println(lpower);
 
-    }
     lcurrent = (long)node.getResponseBuffer(0x0D) / 100.0f;
-    if (debug == 1) {
-      Serial.print("Load Current: ");
-      Serial.println(lcurrent);
 
-    }
     pvvoltage = (long)node.getResponseBuffer(0x00) / 100.0f;
-    if (debug == 1) {
-      Serial.print("PV Voltage: ");
-      Serial.println(pvvoltage);
 
-    }
     pvcurrent = (long)node.getResponseBuffer(0x01) / 100.0f;
-    if (debug == 1) {
-      Serial.print("PV Current: ");
-      Serial.println(pvcurrent);
 
-    }
     pvpower = ((long)node.getResponseBuffer(0x03) << 16 | node.getResponseBuffer(0x02)) / 100.0f;
-    if (debug == 1) {
-      Serial.print("PV Power: ");
-      Serial.println(pvpower);
-    }
+
     battBhargeCurrent = (long)node.getResponseBuffer(0x05) / 100.0f;
-    if (debug == 1) {
-      Serial.print("Battery Charge Current: ");
-      Serial.println(battBhargeCurrent);
-      Serial.println();
-    }
+
   } else {
     rs485DataReceived = false;
   }
@@ -183,40 +149,14 @@ void AddressRegistry_311A() {
   if (result == node.ku8MBSuccess)
   {
     bremaining = node.getResponseBuffer(0x00) / 1.0f;
-    if (debug == 1) {
-      Serial.print("Battery Remaining %: ");
-      Serial.println(bremaining);
 
-    }
     btemp = node.getResponseBuffer(0x01) / 100.0f;
-    if (debug == 1) {
-      Serial.print("Battery Temperature: ");
-      Serial.println(btemp);
-      Serial.println();
-    }
+
   } else {
     rs485DataReceived = false;
   }
 }
 
-void AddressRegistry_3300() {
-  result = node.readInputRegisters(0x3300, 2);
-  if (result == node.ku8MBSuccess)
-  {
-    stats_today_pv_volt_max = node.getResponseBuffer(0x00) / 100.0f;
-    if (debug == 1) {
-      Serial.print("Stats Today PV Voltage MAX: ");
-      Serial.println(stats_today_pv_volt_max);
-    }
-    stats_today_pv_volt_min = node.getResponseBuffer(0x01) / 100.0f;
-    if (debug == 1) {
-      Serial.print("Stats Today PV Voltage MIN: ");
-      Serial.println(stats_today_pv_volt_min);
-    }
-  } else {
-    rs485DataReceived = false;
-  }
-}
 
 
 
